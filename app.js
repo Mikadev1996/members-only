a1efe3714e7590703644466278cad7a3c7590faf
mika_laptop_windows <charmikadev@gmail.com>
2022-07-13 16:13:08 +0100

    project setup

--- app.js
+++ app.js
@@ -1,41 +1,56 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require("express");
+const path = require("path");
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const mongoose = require("mongoose");
+const bcrypt = require('bcryptjs');
+require('dotenv').config();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const User = require('./models/user');
 
-var app = express();
 
-// view engine setup
+const myMongoDB = process.env.MONGODB_URI;
+mongoose.connect(myMongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
+const db = mongoose.connection;
+db.on('error', () => { console.error.bind(console, 'MongoDB connection error') });
+
+const app = express();
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
-app.use(logger('dev'));
-app.use(express.json());
+passport.use(
+    new LocalStrategy((username, password, done) => {
+      User.findOne({username: username}, (err, user) => {
+        if (err) return done(err);
+        if (!user) return done(null, false, { message: "Incorrect Username"});
+
+        bcrypt.compare(password, user.password, (err, res) => {
+          if (res) return done(null, user);
+          else return done(null, false, { message: "Incorrect Password"});
+        })
+      })
+    })
+)
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+})
+
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  })
+})
+
+app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 module.exports = app;
