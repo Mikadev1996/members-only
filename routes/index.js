e68b69c19fdd17631aa7e3c07e33af9264e0414b
Mika.dev <charmikadev@gmail.com>
2022-07-16 20:11:48 +0100

    get messages method, render messages in descending order

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const Message = require('../models/message');
 
-router.get('/', (req, res) => {
-  res.render('index', {
-    title: "Members Only ",
-    user: req.user
-  });
+router.get('/', (req, res, next) => {
+  Message.find()
+      .sort([['timestamp', 'descending']])
+      .exec((err, list_messages) => {
+        if (err) return next(err);
+        res.render('index', {
+          title: "Members Only ",
+          messages: list_messages,
+          user: req.user
+        });
+      })
+
 })
-
 module.exports = router;
